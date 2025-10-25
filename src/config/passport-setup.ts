84e8e364bcfbaa3cf1c127587e585370af42c31f
Syafiq Rohman <syafiqrohman@outlook.com>
2025-10-25 15:28:36 +0700

    feat:fixing passport google OAuth

--- src/config/passport-setup.ts
+++ src/config/passport-setup.ts
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
       scope: ['profile', 'email']
     },
     async (accessToken, refreshToken, profile, done) => {
